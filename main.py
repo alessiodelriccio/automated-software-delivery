b0cd935b936f2a076d47dcea0f5fbf8fc52d21f7
Alessio Del Riccio <a.delriccio1@studenti.unimol.it>
2025-03-27 12:07:48 +0000

    Definizione delle API routes per recupero commit e filtraggio delle commit relative a fix di vulnerabilità

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
+
+TOKEN = os.getenv('ACCESS_TOKEN')
+REPO_NAME = "tensorflow/tensorflow"
+CSV_FILE = 'commit_data.csv'
+CSV_RESULTS_FILE = 'results.csv'
+
+@app.get("/repo-commits")
+async def get_commits():
+    # Implementare funzione che estrae i commit dal repo e salva i dati in un CSV
+    pass
+
+@app.get("/filter-commit")
+async def get_vulnerability_commit():
+    # Implementare funzione che filtra i commit che riguardano vulnerabilità
+    pass
